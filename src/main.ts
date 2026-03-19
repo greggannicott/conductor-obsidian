6c4b8f95f7e9fa29853067758f037d58d1ad28ce
Greg Gannicott <greg.gannicott@precisely.com>
2026-03-19 14:23:23 +0000

    refactor: Remove redundant code from what is essentially the old plugin

--- src/main.ts
+++ src/main.ts
@@ -6,49 +6,15 @@ import { getProjectFromFile, getProjects, Project } from "src/projects";
 import { createNewTask, getTasks, Task } from "./tasks";
 import { ChooseTaskModal } from "./choose-task.modal";
 
-interface ConductorSettings {
-	taskId: number;
-	projectId: number;
-}
+interface ConductorSettings {}
 
-const DEFAULT_SETTINGS: ConductorSettings = {
-	taskId: 0,
-	projectId: 0,
-};
+const DEFAULT_SETTINGS: ConductorSettings = {};
 
 export default class ConductorObsidian extends Plugin {
 	settings: ConductorSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		this.addCommand({
-			id: "insert-task",
-			name: "Insert New Task",
-			hotkeys: [{ modifiers: ["Ctrl"], key: "t" }],
-			editorCallback: async (editor, _) => {
-				await this.loadSettings();
-				// Set new task ID
-				this.settings.taskId++;
-				const taskId = `TSK-${this.settings.taskId}`;
-
-				// Build line
-				let taskText = `- [[${taskId} - ]]`;
-
-				// Insert Task
-				const { line } = editor.getCursor();
-				editor.setLine(line, taskText);
-
-				// Move cursor so you can write the task name immediately
-				const lineLength = editor.getLine(line).length;
-				const taskNameInsertPosition = lineLength - 2;
-				const newCursorPosition = { line, ch: taskNameInsertPosition };
-				editor.setCursor(newCursorPosition);
-
-				// Save new ID
-				this.saveSettings();
-			},
-		});
 
 		this.addCommand({
 			id: "open-project",
