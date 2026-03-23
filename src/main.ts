6cc19202c377309867bb95489c89acf48d600248
Greg Gannicott <greg@gannicott.co.uk>
2026-03-23 22:11:19 +0000

    refactor: Extract functions out of `addCommand` statements
    This is to make it easier to see at a glance which commands exist.

--- src/main.ts
+++ src/main.ts
@@ -19,56 +19,62 @@ export default class ConductorObsidian extends Plugin {
 		this.addCommand({
 			id: "open-project",
 			name: "Open Project",
-			callback: async () => {
-				const selectProjectModal = new ChooseProjectModal(this.app);
-				selectProjectModal.projects = getProjects(this.app);
-				selectProjectModal.onChoose = async (project: Project) => {
-					this.app.workspace.getLeaf(false).openFile(project.file);
-				};
-				selectProjectModal.open();
-			},
+			callback: this.openProject,
 		});
 
 		this.addCommand({
 			id: "open-task",
 			name: "Open Task",
-			callback: async () => {
-				const selectTaskModal = new ChooseTaskModal(this.app);
-				const activeProject = getActiveProject(this.app);
-				const options: getTaskOptions = {
-					project: activeProject,
-				};
-				selectTaskModal.tasks = getTasks(this.app, options);
-				selectTaskModal.onChoose = async (task: Task) => {
-					this.app.workspace.getLeaf(false).openFile(task.file);
-				};
-				selectTaskModal.open();
-			},
+			callback: this.openTask,
 		});
 
 		this.addCommand({
 			id: "create-new-task",
 			name: "Create New Task",
-			callback: async () => {
-				// See if the focussed file is a project. If it is, use that as the chosen project.
-				const activeProject = getActiveProject(this.app);
-
-				if (activeProject) {
-					this.displayTaskNameInput(activeProject);
-				} else {
-					// Obtain a list of possible projects
-					const projects = getProjects(this.app);
-
-					// Display a modal to obtain the chosen project
-					const selectProjectModal = new ChooseProjectModal(this.app);
-					selectProjectModal.projects = projects;
-
-					selectProjectModal.onChoose = this.displayTaskNameInput;
-					selectProjectModal.open();
-				}
-			},
+			callback: this.createNewTask,
 		});
 	}
+
+	openProject = async () => {
+		const selectProjectModal = new ChooseProjectModal(this.app);
+		selectProjectModal.projects = getProjects(this.app);
+		selectProjectModal.onChoose = async (project: Project) => {
+			this.app.workspace.getLeaf(false).openFile(project.file);
+		};
+		selectProjectModal.open();
+	};
+
+	openTask = async () => {
+		const selectTaskModal = new ChooseTaskModal(this.app);
+		const activeProject = getActiveProject(this.app);
+		const options: getTaskOptions = {
+			project: activeProject,
+		};
+		selectTaskModal.tasks = getTasks(this.app, options);
+		selectTaskModal.onChoose = async (task: Task) => {
+			this.app.workspace.getLeaf(false).openFile(task.file);
+		};
+		selectTaskModal.open();
+	};
+
+	createNewTask = async () => {
+		// See if the focussed file is a project. If it is, use that as the chosen project.
+		const activeProject = getActiveProject(this.app);
+
+		if (activeProject) {
+			this.displayTaskNameInput(activeProject);
+		} else {
+			// Obtain a list of possible projects
+			const projects = getProjects(this.app);
+
+			// Display a modal to obtain the chosen project
+			const selectProjectModal = new ChooseProjectModal(this.app);
+			selectProjectModal.projects = projects;
+
+			selectProjectModal.onChoose = this.displayTaskNameInput;
+			selectProjectModal.open();
+		}
+	};
 
 	async displayTaskNameInput(selectedProject: Project) {
 		const keybindings: TextInputKeybinding[] = [
