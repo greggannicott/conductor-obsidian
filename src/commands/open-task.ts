1111fe8060586df0d9dc148e438db940df97213e
Greg Gannicott <greg@gannicott.co.uk>
2026-09-22 16:44:29 +0100

    feat: Broaden in progress project scope to ongoing projects

--- src/commands/open-task.ts
+++ src/commands/open-task.ts
@@ -103,12 +103,17 @@ export const openInProgressTaskFromInProgressProject = async (
 	app: App,
 ): Promise<void> => {
 	const projectFilter: ProjectFilters = {
-		statusFilter: {
-			statusIs: [ProjectStatus.InProgress],
-		},
-		ongoingFilter: {
-			ongoingIs: false,
-		},
+		or: [
+			{
+				ongoingIs: true,
+				statusIsNot: [
+					ProjectStatus.Done,
+					ProjectStatus.Abandoned,
+					ProjectStatus.WontDo,
+				],
+			},
+			{ statusIs: [ProjectStatus.InProgress] },
+		],
 	};
 	const inProgressProjects = getProjects(app, projectFilter);
 
